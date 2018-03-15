d050d71b5c880b0bdc0d00a4749775650cfc2eaf
KostadinovK <kostadinovk2001@gmail.com>
2018-03-15 20:09:06 +0200

    First Commit

---
+++ app/src/main/java/com/theerrors/xames/BasheGameActivity.java
@@ -0,0 +1,14 @@
+package com.theerrors.xames;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+//bashe game main activity
+
+public class BasheGameActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bashe_main);
+    }
+}
---
+++ app/src/main/java/com/theerrors/xames/DudeliGameActivity.java
@@ -0,0 +1,14 @@
+package com.theerrors.xames;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+// game of dudeli main activity
+public class DudeliGameActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dudeli_main);
+    }
+}
