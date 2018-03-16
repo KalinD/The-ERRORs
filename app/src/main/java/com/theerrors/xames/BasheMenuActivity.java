4994af79c39f5c15a11790fee1bb6744dd97d814
KalinD <doiychevk@gmail.com>
2018-03-16 13:39:21 +0200

    added bashe game menu

---
+++ app/src/main/java/com/theerrors/xames/BasheMenuActivity.java
@@ -0,0 +1,39 @@
+package com.theerrors.xames;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+public class BasheMenuActivity extends AppCompatActivity {
+    @BindView(R.id.btn_one_player)
+    Button mBtnOnePlayer;
+    @BindView(R.id.btn_two_players)
+    Button mBtnTwoPlayers;
+    @BindView(R.id.btn_back)
+    Button mBtnBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bashe_menu);
+    }
+    Intent intent = new Intent(BasheMenuActivity.this, BasheGameActivity.class);
+    @OnClick(R.id.btn_one_player)
+    public void OnePlayerGameClicked(){
+        intent.putExtra("PlayerAmount", 1);
+        startActivity(intent);
+    }
+    @OnClick(R.id.btn_two_players)
+    public void TwoPlayersGameClicked(){
+        intent.putExtra("PlayerAmount", 2);
+        startActivity(intent);
+    }
+    @OnClick(R.id.btn_back)
+    public void BackButtonClicked(){
+        intent = new Intent(BasheMenuActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
