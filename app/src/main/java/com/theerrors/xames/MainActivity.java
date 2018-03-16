e922ce5211c1f4c121d07aa0c0c339b07e72eaf7
KostadinovK <kostadinovk2001@gmail.com>
2018-03-16 08:58:28 +0200

    added main menu skeleton

--- app/src/main/java/com/theerrors/xames/MainActivity.java
+++ app/src/main/java/com/theerrors/xames/MainActivity.java
@@ -1,13 +1,35 @@
 package com.theerrors.xames;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
-
+    @BindView(R.id.iv_game_one) ImageView iv_game_one;
+    @BindView(R.id.iv_game_two) ImageView iv_game_two;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+    @OnClick(R.id.iv_game_one)
+    public void onClickGameOne(){
+        Intent intent = new Intent(this,BasheGameActivity.class);
+        startActivity(intent);
+
+    }
+
+    @OnClick(R.id.iv_game_two)
+    public void OnClickImageTwo(){
+        Intent intent = new Intent(this,DudeliGameActivity.class);
+        startActivity(intent);
+
     }
 }
