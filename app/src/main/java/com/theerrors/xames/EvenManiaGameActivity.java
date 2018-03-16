8101fa3ad86b23110e9bc41b72f3596d72fef2a8
KostadinovK <kostadinovk2001@gmail.com>
2018-03-16 10:23:33 +0200

    disabling screen rotating, main menu design improvements

---
+++ app/src/main/java/com/theerrors/xames/EvenManiaGameActivity.java
@@ -0,0 +1,17 @@
+package com.theerrors.xames;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+
+
+public class EvenManiaGameActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_even_mania_main);
+        ButterKnife.bind(this);
+    }
+}
