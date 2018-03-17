39e16871f2c9230fae731237875b7d047b9d0f6d
marto_mate <mar.penev@gmail.com>
2018-03-17 14:38:41 +0200

    Working graph and action control on EvenMania

--- app/src/main/java/com/theerrors/xames/EvenManiaGameActivity.java
+++ app/src/main/java/com/theerrors/xames/EvenManiaGameActivity.java
@@ -1,9 +1,12 @@
 package com.theerrors.xames;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -22,34 +25,61 @@ import butterknife.OnTextChanged;
 public class EvenManiaGameActivity extends AppCompatActivity {
     EditText choice1;
     EditText choice2;
-    int p1_choice, p2_choice;
-    int moveing_thing_x = 0, moveing_thing_y = 0;
+    int p1_choice_num, p2_choice_num;
+    int moving_thing_x = 0, moving_thing_y = 0;
+    int boundary_value = 6; //for now
+    int one_graphBox_size = 154;
+
+    ImageView image_moving_thing;
+
+
+    public void CheckWin(){
+        if(moving_thing_x - moving_thing_y >= boundary_value * one_graphBox_size){
+            final AlertDialog.Builder builder = new AlertDialog.Builder(EvenManiaGameActivity.this);
+            builder.setMessage("Someone won!!");
+            builder.setTitle("Game Over!");
+            builder.setNegativeButton("Okay", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Intent intent = new Intent(EvenManiaGameActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            });
+            AlertDialog endGame = builder.create();
+            endGame.show();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_even_mania_main);
         ButterKnife.bind(this);
+
+
     }
+
+    @BindView(R.id.p1_choice) EditText p1_choice;
+    @BindView(R.id.p2_choice) EditText p2_choice;
 
     @OnClick(R.id.p1_choice)
     public void onPlayer1Input() {
         choice1 = (EditText) findViewById(R.id.p1_choice);
-        choice2 = (EditText) findViewById(R.id.p2_choice);
+
+        ImageView image_moving_thing = (ImageView) findViewById(R.id.image_moving_thing);
 
         String p1Value = choice1.getText().toString();
         if (p1Value.length() >= 1) {
-            p1_choice = Integer.parseInt(p1Value);
-            Log.d("EvenMania", "" + p1_choice);
+            p1_choice_num = Integer.parseInt(p1Value);
 
-            // EXPERIMENT
-            ImageView image_moving_thing = (ImageView) findViewById(R.id.image_moving_thing);
 
-            TranslateAnimation moveLefttoRight = new TranslateAnimation(moveing_thing_x + 0,moveing_thing_x + 100, moveing_thing_y + 0, moveing_thing_y + 0);
-            moveLefttoRight.setDuration(1000);
-            moveLefttoRight.setFillAfter(true);
+            TranslateAnimation move = new TranslateAnimation(moving_thing_x + 0,moving_thing_x + (one_graphBox_size * p1_choice_num), moving_thing_y + 0, moving_thing_y + 0);
+            move.setDuration(1000);
+            move.setFillAfter(true);
 
-            image_moving_thing.startAnimation(moveLefttoRight);
+            image_moving_thing.startAnimation(move);
+            moving_thing_x += one_graphBox_size * p1_choice_num;
+            CheckWin();
         }
 
     }
@@ -57,25 +87,38 @@ public class EvenManiaGameActivity extends AppCompatActivity {
     @OnClick(R.id.p2_choice)
     public void onPlayer2Input(){
 
+        choice2 = (EditText) findViewById(R.id.p2_choice);
         String p2Value = choice2.getText().toString();
+        ImageView image_moving_thing = (ImageView) findViewById(R.id.image_moving_thing);
+
         if(p2Value.length() >= 1){
-            p2_choice = Integer.parseInt(p2Value);
-            Log.d("EvenMania", "" + p2_choice);
-            // EXPERIMENT
-            ImageView image_moving_thing = (ImageView) findViewById(R.id.image_moving_thing);
+            p2_choice_num = Integer.parseInt(p2Value);
 
-            TranslateAnimation moveLefttoRight = new TranslateAnimation(moveing_thing_x + 0,moveing_thing_x + 0 , moveing_thing_y + 0, moveing_thing_y + 100);
-            moveLefttoRight.setDuration(1000);
-            moveLefttoRight.setFillAfter(true);
 
-            image_moving_thing.startAnimation(moveLefttoRight);
+            int predicted_location = moving_thing_y - one_graphBox_size*p2_choice_num;
+
+            if(-predicted_location + moving_thing_x >= boundary_value * one_graphBox_size) {
+
+                TranslateAnimation move = new TranslateAnimation(moving_thing_x + 0,moving_thing_x + 0 , moving_thing_y + 0, moving_thing_y - (boundary_value * p2_choice_num - one_graphBox_size * p2_choice_num));
+                move.setDuration(1000);
+                move.setFillAfter(true);
+                image_moving_thing.startAnimation(move);
+                CheckWin();
+
+            }else{
+
+                TranslateAnimation move = new TranslateAnimation(moving_thing_x + 0,moving_thing_x + 0 , moving_thing_y + 0, moving_thing_y - (one_graphBox_size * p2_choice_num));
+                move.setDuration(1000);
+                move.setFillAfter(true);
+                image_moving_thing.startAnimation(move);
+
+            }
+
+
+
+            moving_thing_y -= one_graphBox_size * p2_choice_num;
         }
 
-
-    }
-
-
-    public void MoveObject() {
 
     }
 
